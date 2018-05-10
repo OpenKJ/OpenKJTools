5778c41286a740c33e5eb8b4b063a5843974caae
Isaac Lightburn <isaac@openkj.org>
2018-05-10 13:54:56 -0500

    Fix KaraokeRG not being able to replace files on Windows

--- version.h
+++ version.h
@@ -3,8 +3,8 @@
 
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 11
-#define VERSION_BUILD 14
-#define VERSION_STRING "0.11.14"
+#define VERSION_BUILD 15
+#define VERSION_STRING "0.11.15"
 #define VERSION_BRANCH "unstable"
 
 #endif //VERSION_H
