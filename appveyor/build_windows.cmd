6926e59ff156d5f1ff5d1154807179a95b2db84f
Isaac Lightburn <lightburnisaac@psychopath.hm.hozed.net>
2020-05-01 22:43:50 -0500

    fixing windows builds

--- appveyor/build_windows.cmd
+++ appveyor/build_windows.cmd
@@ -42,7 +42,7 @@ robocopy release\ "%project_dir%\output" /E /np
 del "%project_dir%\output\*.obj"
 del "%project_dir%\output\*.cpp"
 del "%project_dir%\output\*.h"
-7z e "%project_dir%\appveyor\ffmpeg-win-32.zip -o"%project_dir%\output\"
+7z e "%project_dir%\ffmpeg-win-32.zip -o"%project_dir%\output\"
 
 
 echo Creating installer...
